45d1255ae5cd13165d6a8fb44c72e59193a1a6d7
Lucho Suarez <luiss@luchos.local>
2019-08-22 11:40:50 -0700

    adding appbar to show material ui usage, on home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Typography,
+  Toolbar,
+} from '@material-ui/core'
+
+import MenuIcon from '@material-ui/icons/Menu'
 
 export default () => (
   <header className="header">
+    <AppBar />
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">Hello</Typography>
+      </Toolbar>
+    </AppBar>
     <img
       src="https://luchoster.dev/icons/icon-256x256.png"
       alt="Luchoster's logo"
